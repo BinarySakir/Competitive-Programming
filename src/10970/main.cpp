02ed88bc2552b095fbece94edbd9daafe2e1f6fc
BinarySakir <binarysakir@gmail.com>
2015-06-06 22:34:35 +0600

    Inital Commit

---
+++ src/10970/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+
+    int x, y, split_count;
+    while(cin >> x >> y){
+        split_count = (x*y)-1;
+        cout << split_count << endl;
+    }
+    return 0;
+
+}
