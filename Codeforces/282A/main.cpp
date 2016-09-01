6253c44a3c1c1f4207c286556b680307e428e43d
binarysakir <binarysakir@gmail.com>
2016-09-01 23:22:45 +0600

    Fixed WA

--- Codeforces/282A/main.cpp
+++ Codeforces/282A/main.cpp
@@ -3,15 +3,18 @@
 using namespace std;
 
 int main(){
-	int n, x = 0;
-	char c[3];
+	int n;
+	int x = 0;
+	string c;
 	cin >> n;
 	while(n--){
 		cin >> c;
-		if(c[1] == '+') x = x + 1;
-		else x = x - 1;
+		if(c[1] == '+'){
+			++x;
+		}else{
+			--x;
+		}
 	}
 	cout << x << endl;
-	system("pause");
 	return 0;
 }
